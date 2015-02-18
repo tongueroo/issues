f1724d9f438ac9c83f40b929e279e73a5fef1ae7
Tung Nguyen <tongueroo@gmail.com>
2015-02-17 19:14:28 -0800

    add sort method

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -1,13 +1,26 @@
 defmodule Issues.GithubIssues do
   @user_agent ["User-agent": "Elixir tung@email.com"]
+  @github_url Application.get_env(:issues, :github_url)
 
-  # Issues.GithubIssues.fetch("elixir-lang", "elixir", 5)
+  def github_url2 do
+    @github_url2
+  end
+  
+
+  # list = Issues.GithubIssues.fetch("elixir-lang", "elixir", 5)
   def fetch(user, project, count) do
     o = issues_url(user, project)
       |> HTTPoison.get(@user_agent)
       |> handle_response
+      |> convert_to_hash_dict
+      |> sort_asc
     # IO.inspect o
   end
+
+  def sort_asc(list) do
+    Enum.sort(list, fn(a,b) -> a["created_at"] >= b["created_at"] end)
+  end
+  
 
   def handle_response({:ok, %HTTPoison.Response{body: body}}) do
     { :ok, :jsx.decode(body) }
@@ -25,7 +38,7 @@ defmodule Issues.GithubIssues do
   
 
   defp issues_url(user, project) do
-    url = "https://api.github.com/repos/#{user}/#{project}/issues"
+    url = "#{@github_url}/repos/#{user}/#{project}/issues"
     # IO.inspect url
     url
   end
